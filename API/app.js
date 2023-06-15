2371ed8c847ecd099757d89c4f163955c6d08399
Manfreo <51626803+Mansafreo@users.noreply.github.com>
2023-06-16 00:51:39 +0300

    entry point

---
+++ API/app.js
@@ -0,0 +1,25 @@
+//To require express
+const express = require('express');
+const app = express();//To initialize express
+
+
+//Middleware
+//static assets
+app.use(express.static('./public'));// this is a middleware
+app.use(express.urlencoded({extended:false}));// this is a middleware
+app.use(express.json());// this is a middleware to parse json data from the body of the request object
+
+//To require the different routes
+app.use('/register',require('./routes/register.js'));//For user registration
+
+//To start the server on port 5000
+app.listen(5000, (err) => {
+    if (err) {
+        console.log("Error in running server");
+        return;
+    }
+    else{
+    console.log("Server is running on port 5000");
+    }
+}
+);
